74bc051df5f36ce7f6b5a348f39654606c4dc743
Jim Bennett <jimbobbennett@mac.com>
2016-05-21 22:32:44 +1200

    Added templates

---
+++ Template/MVVMCross WPF Application/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Wpf.Views;
+
+namespace $safeprojectname$.Views
+{
+    public partial class FirstView : MvxWpfView
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Template/MVVMCross Windows Store Phone Application/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.WindowsCommon.Views;
+
+namespace $safeprojectname$.Views
+{
+    public sealed partial class FirstView : MvxWindowsPage
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
