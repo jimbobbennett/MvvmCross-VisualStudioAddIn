f14bbe9adbdad6976d306ad788c7a41c00c4339e
Jim Bennett <jimbobbennett@mac.com>
2016-05-21 22:12:33 +1200

    Shortening names

---
+++ MvvmCross-VSAddIn.Wpf/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Wpf.Views;
+
+namespace MvvmCross_VSAddIn.Wpf.Views
+{
+    public partial class FirstView : MvxWpfView
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ MvvmCross-VSAddIn.iOS/Views/FirstView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.iOS.Views;
+using MvvmCross_VSAddIn.Core.ViewModels;
+
+namespace MvvmCross_VSAddIn.iOS.Views
+{
+    [MvxFromStoryboard]
+    public partial class FirstView : MvxViewController<FirstViewModel>
+    {
+        public FirstView(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var set = this.CreateBindingSet<FirstView, FirstViewModel>();
+            set.Bind(Label).To(vm => vm.Hello);
+            set.Bind(TextField).To(vm => vm.Hello);
+            set.Apply();
+        }
+    }
+}
