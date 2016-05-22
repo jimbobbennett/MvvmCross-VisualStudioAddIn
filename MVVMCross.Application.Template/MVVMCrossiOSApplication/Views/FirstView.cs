bce1749e15833f80456d8e823264277344eb5ad2
Jim Bennett <jimbobbennett@mac.com>
2016-05-22 19:56:15 +1200

    Working multi project template

--- MVVMCross.Application.Template/MVVMCrossiOSApplication/Views/FirstView.cs
+++ MVVMCross.Application.Template/MVVMCrossiOSApplication/Views/FirstView.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using MvvmCross.Binding.BindingContext;
 using MvvmCross.iOS.Views;
-using MvvmCross_VSAddIn.Core.ViewModels;
+using $ext_safeprojectname$.Core.ViewModels;
 
 namespace $safeprojectname$.Views
 {
