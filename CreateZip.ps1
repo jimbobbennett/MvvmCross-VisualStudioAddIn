4c302327833e773a2b481af4fb0b7fd3a0e4d235
Jim Bennett <jimbobbennett@mac.com>
2016-06-18 18:54:34 +1200

    Upgrade to 4.2

---
+++ CreateZip.ps1
@@ -0,0 +1,5 @@
+﻿Add-Type -Assembly "System.IO.Compression.FileSystem";
+$sourceDir =  $PSScriptRoot + "\MVVMCross.Application.Template";
+$outFile = $PSScriptRoot + "\MVVMCross-VSAddIn\ProjectTemplates\CSharp\MvvmCross\Root.zip";
+Remove-Item $outFile;
+[System.IO.Compression.ZipFile]::CreateFromDirectory($sourceDir, $outFile);
--- MvvmCross-VSAddIn.Droid/Bootstrap/JsonPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace MvvmCross_VSAddIn.Droid.Bootstrap
-{
-    public class JsonPluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.Json.PluginLoader>
-    {
-    }
-}
